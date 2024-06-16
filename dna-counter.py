17826357e467468d1b3722c60823cd68f69349af
Izet Delibasic <izetdelibasic55@gmail.com>
2024-06-16 22:34:37 +0200

    Creating data frame and chart bars for statistics

--- dna-counter.py
+++ dna-counter.py
@@ -42,4 +42,31 @@ X= DNA_nucletiode_count(sequence)
 
 X
 
+# Counter Informations
+st.subheader('2. Print Text')
+st.write('There are  ' + str(X['A']) + ' adenine (A)')
+st.write('There are  ' + str(X['T']) + ' thymine (T)')
+st.write('There are  ' + str(X['G']) + ' guanine (G)')
+st.write('There are  ' + str(X['C']) + ' cytosine (C)')
+
+# DataFrame Display
+st.subheader('3. Display DataFrame')
+df = pd.DataFrame.from_dict(X, orient="index")
+df = df.rename({0: 'count'}, axis="columns")
+df.reset_index(inplace=True)
+df =df.rename(columns = {"index":"nucleotide"})
+st.write(df)
+
+# BarChart Display
+
+st.subheader("4. Display Bar Chart")
+p = alt.Chart(df).mark_bar().encode(
+    x="nucleotide",
+    y="count"
+)
+p = p.properties(
+    width = alt.Step(100)
+)
+st.write(p)
+
 
